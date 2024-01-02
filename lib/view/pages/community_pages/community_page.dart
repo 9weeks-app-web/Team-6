43dc5f0e659c64c2806639355bf29ef8f53947ae
Ko-Eunseo <koyk0408@naver.com>
2024-01-02 16:23:00 +0900

    Fix: dev pull 후 에러 발생. Height값 추가

--- lib/view/pages/community_pages/community_page.dart
+++ lib/view/pages/community_pages/community_page.dart
@@ -11,42 +11,45 @@ class CommunityPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 10,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 10,
+              ),
+              child: HeaderWidget(),
             ),
-            child: HeaderWidget(),
-          ),
-          const SizedBox(height: 18),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  '커뮤니티',
-                  style: DesignTextStyle.HeadLine_Bold,
+            const SizedBox(height: 18),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    '커뮤니티',
+                    style: DesignTextStyle.HeadLine_Bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: SearchBarWidget(),
-              ),
-              const SizedBox(height: 16),
-            ],
-          ),
-          Expanded(
-            child: TabWidget(
-              tabContents: communityTabList,
+                const SizedBox(height: 8),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: SearchBarWidget(),
+                ),
+                const SizedBox(height: 16),
+              ],
             ),
-          ),
-        ],
+            Expanded(
+              child: TabWidget(
+                tabContents: communityTabList,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
