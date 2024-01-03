b99cc19042be4514da0148f7b4fe9fcac25beed7
Ko-Eunseo <koyk0408@naver.com>
2024-01-03 18:43:43 +0900

    Design: 클럽 탭 - 라운지, 채팅 섹션 구현, 키워드, 투표 섹션 세팅 #20

---
+++ lib/view/pages/community_pages/detail_pages/detail_tabs.dart/keyword_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KeywordTab extends StatelessWidget {
+  const KeywordTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
