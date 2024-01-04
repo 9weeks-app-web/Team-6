496c41d4568f8b63ea16fe470dc9a45d61f66838
Ko-Eunseo <koyk0408@naver.com>
2024-01-04 16:22:36 +0900

    Docs: 키워드 채팅 더미데이터 세팅 #20

---
+++ lib/util/dummy_data/keyword_chatting_data.dart
@@ -0,0 +1,73 @@
+const keywordChatData = [
+  {
+    'nickname': '굿입니다',
+    'message':
+        '안녕하세요! 이제 막 UXUI디자인 공부를 시작한 입문자 입니다. 능력자 분들께 제 포트폴리오 피드백을 부탁드리고 싶은데 가능한 분 계신가요?',
+    'time': '1시간 전',
+    'keyword': '포트폴리오',
+    'like': 13,
+    'comment': [
+      {
+        'nickname': 'Name',
+        'comment': '링크주세요.',
+        'time': 'min ago',
+        'cocomment': {
+          'nickname': 'Name',
+          'comment': '구글링크',
+          'time': 'min ago',
+        }
+      },
+    ]
+  },
+  {
+    'nickname': '햄바오',
+    'message': '''포폴 ..~ 엇떻캐 만드는 거에욤? 더보기
+                \n이거 보여주려고 어그로 끌었다
+                \n포폴 작성 팁 보고가세용
+                \nhttps://yozm.wishket.com/magazine/detail/1685/''',
+    'time': '4일 전',
+    'keyword': '포트폴리오',
+    'like': 324,
+    'comment': [
+      {
+        'nickname': 'Name',
+        'comment': '링크주세요.',
+        'time': 'min ago',
+      },
+      {
+        'nickname': 'Name',
+        'comment': '구글링크',
+        'time': 'min ago',
+      },
+    ]
+  },
+  {
+    'nickname': '슬플때힙합을춰',
+    'message': '엉엉엉 덩실덩실 엉엉엉 웨이브 부리부리',
+    'time': '4일 전',
+    'keyword': '포트폴리오',
+    'like': 8,
+    'comment': [
+      {
+        'nickname': 'Name',
+        'comment': '링크주세요.',
+        'time': 'min ago',
+        'cocomment': {
+          'nickname': 'Name',
+          'comment': '구글링크',
+          'time': 'min ago',
+        }
+      },
+      {
+        'nickname': 'Name',
+        'comment': '링크주세요.',
+        'time': 'min ago',
+        'cocomment': {
+          'nickname': 'Name',
+          'comment': '구글링크',
+          'time': 'min ago',
+        }
+      },
+    ]
+  },
+];
