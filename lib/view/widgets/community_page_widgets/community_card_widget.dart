448d6a2f9becf255d71e981fc7cd3bed88fcbbeb
Ko-Eunseo <koyk0408@naver.com>
2024-01-04 18:40:43 +0900

    Feat: 커뮤니티 카드 위젯에 페이지 이동 기능 추가 #20

--- lib/view/widgets/community_page_widgets/community_card_widget.dart
+++ lib/view/widgets/community_page_widgets/community_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common.dart';
 import 'package:flutter_app/view/widgets/community_page_widgets/chip_widget.dart';
+import 'package:go_router/go_router.dart';
 
 enum CardWidth {
   wMax,
@@ -38,111 +39,119 @@ class CommunityCardWidget extends StatelessWidget {
       '꼬순내',
     ];
 
-    return Container(
-      width: width == CardWidth.wMax ? MediaQuery.of(context).size.width : 210,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(
-          color: DesignColor.Neutral.shade20,
+    return GestureDetector(
+      onTap: () {
+        context.go(
+          '/community/club/:clubId',
+        );
+      },
+      child: Container(
+        width:
+            width == CardWidth.wMax ? MediaQuery.of(context).size.width : 210,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: DesignColor.Neutral.shade20,
+          ),
         ),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // img section
-          Container(
-            width: width == CardWidth.wMax
-                ? MediaQuery.of(context).size.width
-                : 210,
-            height: 100,
-            decoration: BoxDecoration(
-              color: DesignColor.Neutral.shade20,
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(8),
-                topRight: Radius.circular(8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // img section
+            Container(
+              width: width == CardWidth.wMax
+                  ? MediaQuery.of(context).size.width
+                  : 210,
+              height: 100,
+              decoration: BoxDecoration(
+                color: DesignColor.Neutral.shade20,
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(8),
+                  topRight: Radius.circular(8),
+                ),
               ),
             ),
-          ),
-          // description section
-          Padding(
-            padding: width == CardWidth.wMax
-                ? const EdgeInsets.all(16)
-                : const EdgeInsets.all(12),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  clubName,
-                  style: width == CardWidth.wMax
-                      ? DesignTextStyle(
-                              style: DesignStyle.SubTitle_SemiBold,
-                              color: DesignColor.Neutral)
-                          .textStyle
-                      : DesignTextStyle(
-                              style: DesignStyle.Body_SemiBold,
-                              color: DesignColor.Neutral)
-                          .textStyle,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  description,
-                  style: DesignTextStyle(
-                          style: DesignStyle.Label_2,
-                          color: DesignColor.Neutral.shade40)
-                      .textStyle,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                ),
-                type == CardType.growth
-                    ? const SizedBox(height: 8)
-                    : const SizedBox(height: 12),
-                Wrap(
-                  spacing: 6,
-                  children: type == CardType.growth
-                      ? jobs
-                          .map(
-                            (job) => ChipWidget(
-                              label: job,
-                            ),
-                          )
-                          .toList()
-                      : tags
-                          .map(
-                            (tag) => ChipWidget(
-                              type: ChipType.tag,
-                              label: tag,
-                            ),
-                          )
-                          .toList(),
-                ),
-                const SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: RichText(
-                    text: TextSpan(
-                      text: '$headcount',
-                      style: DesignTextStyle(
-                        style: DesignStyle.Label_3_SemiBold,
-                        color: DesignColor.Primary.shade80,
-                      ).textStyle,
-                      children: [
-                        TextSpan(
-                          text: '/$max명',
-                          style: DesignTextStyle(
-                            style: DesignStyle.Caption_1_Medium,
-                            color: DesignColor.Neutral.shade60,
-                          ).textStyle,
-                        ),
-                      ],
+            // description section
+            Padding(
+              padding: width == CardWidth.wMax
+                  ? const EdgeInsets.all(16)
+                  : const EdgeInsets.all(12),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    clubName,
+                    style: width == CardWidth.wMax
+                        ? DesignTextStyle(
+                                style: DesignStyle.SubTitle_SemiBold,
+                                color: DesignColor.Neutral)
+                            .textStyle
+                        : DesignTextStyle(
+                                style: DesignStyle.Body_SemiBold,
+                                color: DesignColor.Neutral)
+                            .textStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    description,
+                    style: DesignTextStyle(
+                            style: DesignStyle.Label_2,
+                            color: DesignColor.Neutral.shade40)
+                        .textStyle,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                  type == CardType.growth
+                      ? const SizedBox(height: 8)
+                      : const SizedBox(height: 12),
+                  Wrap(
+                    spacing: 6,
+                    children: type == CardType.growth
+                        ? jobs
+                            .map(
+                              (job) => ChipWidget(
+                                label: job,
+                              ),
+                            )
+                            .toList()
+                        : tags
+                            .map(
+                              (tag) => ChipWidget(
+                                type: ChipType.tag,
+                                label: tag,
+                              ),
+                            )
+                            .toList(),
+                  ),
+                  const SizedBox(height: 10),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: RichText(
+                      text: TextSpan(
+                        text: '$headcount',
+                        style: DesignTextStyle(
+                          style: DesignStyle.Label_3_SemiBold,
+                          color: DesignColor.Primary.shade80,
+                        ).textStyle,
+                        children: [
+                          TextSpan(
+                            text: '/$max명',
+                            style: DesignTextStyle(
+                              style: DesignStyle.Caption_1_Medium,
+                              color: DesignColor.Neutral.shade60,
+                            ).textStyle,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
