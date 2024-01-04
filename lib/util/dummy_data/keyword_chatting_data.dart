62abe519a472c82e471e33f7345c5dbf437def0b
Ko-Eunseo <koyk0408@naver.com>
2024-01-04 18:39:02 +0900

    Docs: 키워드 채팅 데이터 세팅 #20

--- lib/util/dummy_data/keyword_chatting_data.dart
+++ lib/util/dummy_data/keyword_chatting_data.dart
@@ -1,5 +1,6 @@
 const keywordChatData = [
   {
+    'id': '1',
     'nickname': '굿입니다',
     'message':
         '안녕하세요! 이제 막 UXUI디자인 공부를 시작한 입문자 입니다. 능력자 분들께 제 포트폴리오 피드백을 부탁드리고 싶은데 가능한 분 계신가요?',
@@ -10,16 +11,30 @@ const keywordChatData = [
       {
         'nickname': 'Name',
         'comment': '링크주세요.',
-        'time': 'min ago',
-        'cocomment': {
-          'nickname': 'Name',
-          'comment': '구글링크',
-          'time': 'min ago',
-        }
+        'time': '5min ago',
+        'cocomment': [
+          {
+            'nickname': 'Name',
+            'comment': '구글링크',
+            'time': '5min ago',
+          },
+          {
+            'nickname': 'Name',
+            'comment': '구글링크',
+            'time': 'min ago',
+          },
+        ],
       },
+      {
+        'nickname': 'Name2',
+        'comment': '저도 주세요',
+        'time': '2min ago',
+        'cocomment': [],
+      }
     ]
   },
   {
+    'id': '2',
     'nickname': '햄바오',
     'message': '''포폴 ..~ 엇떻캐 만드는 거에욤? 더보기
                 \n이거 보여주려고 어그로 끌었다
@@ -42,6 +57,7 @@ const keywordChatData = [
     ]
   },
   {
+    'id': '3',
     'nickname': '슬플때힙합을춰',
     'message': '엉엉엉 덩실덩실 엉엉엉 웨이브 부리부리',
     'time': '4일 전',
