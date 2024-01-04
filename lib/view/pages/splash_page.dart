349a744a8b8f372a1d66046b6efb82bd04361e37
hojin9car <hojin9car@gmail.com>
2024-01-04 14:05:27 +0900

    splash_screen.dart

--- lib/view/pages/splash_page.dart
+++ lib/view/pages/splash_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(const SplashScreen());
+// }
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(bottom: 20),
+          child: Center(
+            child: Image.asset(
+              'assets/logo/spac_splash.png',
+              width: 200,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
