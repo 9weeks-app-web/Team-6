fc617337eeb6b5a530420c55d8dff6f790880713
Ko-Eunseo <koyk0408@naver.com>
2024-01-08 21:57:45 +0900

    Remove: 사용하지 않는 import 삭제

--- lib/view/pages/community_pages/detail_pages/recruit_detail_page.dart
+++ lib/view/pages/community_pages/detail_pages/recruit_detail_page.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common.dart';
 import 'package:flutter_app/util/community/recruit_detail_data.dart';
-import 'package:flutter_app/view/widgets/community_page_widgets/rich_text_with_divider_widget.dart';
-import 'package:flutter_app/view/widgets/community_page_widgets/subtitle_widget.dart';
 import 'package:flutter_app/view/widgets/recruit_page_widgets/detail_sections/company_info_section.dart';
 import 'package:flutter_app/view/widgets/recruit_page_widgets/detail_sections/recruit_description_section.dart';
 import 'package:flutter_app/view/widgets/recruit_page_widgets/detail_sections/recruit_recommand_section.dart';
 import 'package:flutter_app/view/widgets/recruit_page_widgets/detail_sections/stastics_section.dart';
 import 'package:flutter_app/view/widgets/recruit_page_widgets/floating_recruit_button.dart';
-import 'package:flutter_app/view/widgets/recruit_page_widgets/recruit_card_widget.dart';
 
 class RecruitDetailPage extends StatelessWidget {
   const RecruitDetailPage({
