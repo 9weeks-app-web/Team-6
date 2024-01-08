5f5bf1060b8a991532a3c995ef10321073e90162
Ko-Eunseo <koyk0408@naver.com>
2024-01-08 18:05:29 +0900

    Refactor: 위젯 확장(textStyle 받도록 수정) #33

--- lib/view/widgets/community_page_widgets/rich_text_with_divider_widget.dart
+++ lib/view/widgets/community_page_widgets/rich_text_with_divider_widget.dart
@@ -6,34 +6,39 @@ class RichTextWithDividerWidget extends StatelessWidget {
     super.key,
     required this.frontText,
     required this.secondText,
+    this.textStyle,
   });
 
   final String frontText;
   final String secondText;
+  final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
     return RichText(
       text: TextSpan(
         text: frontText,
-        style: DesignTextStyle(
-          style: DesignStyle.Caption_1_Medium,
-          color: DesignColor.Neutral.shade60,
-        ).textStyle,
+        style: textStyle ??
+            DesignTextStyle(
+              style: DesignStyle.Caption_1_Medium,
+              color: DesignColor.Neutral.shade60,
+            ).textStyle,
         children: [
           TextSpan(
             text: ' | ',
-            style: DesignTextStyle(
-              style: DesignStyle.Caption_1_Medium,
-              color: DesignColor.Neutral.shade60,
-            ).textStyle,
+            style: textStyle ??
+                DesignTextStyle(
+                  style: DesignStyle.Caption_1_Medium,
+                  color: DesignColor.Neutral.shade60,
+                ).textStyle,
           ),
           TextSpan(
             text: secondText,
-            style: DesignTextStyle(
-              style: DesignStyle.Caption_1_Medium,
-              color: DesignColor.Neutral.shade60,
-            ).textStyle,
+            style: textStyle ??
+                DesignTextStyle(
+                  style: DesignStyle.Caption_1_Medium,
+                  color: DesignColor.Neutral.shade60,
+                ).textStyle,
           ),
         ],
       ),
