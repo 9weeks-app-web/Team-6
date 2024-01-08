9e6b0028be652665356014a9ce30f6ff0a95ba37
Ko-Eunseo <koyk0408@naver.com>
2024-01-08 18:06:00 +0900

    Refactor: 위젯 확장(ratio 받도록 수정) #33

--- lib/view/widgets/community_page_widgets/table_widget.dart
+++ lib/view/widgets/community_page_widgets/table_widget.dart
@@ -3,35 +3,51 @@ import 'package:flutter_app/common.dart';
 
 class TableWidget extends StatelessWidget {
   const TableWidget({
-    super.key,
+    Key? key,
     required this.keyValInfo,
-  });
+    this.keyRatio = 0.3,
+  }) : super(key: key);
 
   final Map<String, String> keyValInfo;
+  final double? keyRatio;
 
   @override
   Widget build(BuildContext context) {
     return Table(
-      children: keyValInfo.entries.map(
-        (entry) {
+      columnWidths: {
+        0: FractionColumnWidth(keyRatio!),
+        1: FractionColumnWidth(1 - keyRatio!),
+      },
+      children: List.generate(
+        keyValInfo.length,
+        (index) {
+          final entry = keyValInfo.entries.elementAt(index);
           return TableRow(
             children: [
               TableCell(
-                child: Text(
-                  entry.key,
-                  style: DesignTextStyle.Label_2_Regular,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      bottom: index != keyValInfo.length - 1 ? 12.0 : 0),
+                  child: Text(
+                    entry.key,
+                    style: DesignTextStyle.Label_2_Regular,
+                  ),
                 ),
               ),
               TableCell(
-                child: Text(
-                  entry.value,
-                  style: DesignTextStyle.Label_2_Regular,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      bottom: index != keyValInfo.length - 1 ? 12.0 : 0),
+                  child: Text(
+                    entry.value,
+                    style: DesignTextStyle.Label_2_Regular,
+                  ),
                 ),
               ),
             ],
           );
         },
-      ).toList(),
+      ),
     );
   }
 }
