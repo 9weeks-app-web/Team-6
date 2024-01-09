1fe9b3bec4ea63023bd4ddcfac763a19d392e128
Ko-Eunseo <koyk0408@naver.com>
2024-01-09 22:22:16 +0900

    Design: 필터 페이지 및 탭 리스트 #36

---
+++ lib/view/pages/community_pages/recruit_filter_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/common.dart';
+import 'package:flutter_app/util/community/recruit_filter_tab_list.dart';
+import 'package:flutter_app/view/widgets/community_page_widgets/tab_widget.dart';
+import 'package:flutter_app/view/widgets/recruit_page_widgets/search_bar_widget.dart';
+
+class RecruitFilterPage extends StatelessWidget {
+  const RecruitFilterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: DesignColor.Neutral,
+        ),
+        title: const SearchBarWidget(),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          Expanded(
+            child: TabWidget(tabContents: recruitFilterTabList),
+          ),
+        ],
+      ),
+    );
+  }
+}
