2ca597491d490f7426f3368cb0ded05de3fbc145
Ko-Eunseo <koyk0408@naver.com>
2024-01-09 22:20:09 +0900

    Design: BasicFilterTabbarView 사용한 탭바뷰 #36

---
+++ lib/view/pages/community_pages/filter_pages/fields_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/util/dummy_data/filtering_data.dart';
+import 'package:flutter_app/view/widgets/recruit_page_widgets/basic_filter_tabbar_view.dart';
+
+class FieldsPage extends StatelessWidget {
+  const FieldsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String title = '분야 선택';
+    List<String> selectedJobList = [
+      '3D디자이너',
+      '웹 디자이너',
+      '인테리어 디자이너',
+      'UI,GUI 디자이너',
+      'UX디자이너',
+    ];
+    String selectedJob = '디자인';
+
+    return BasicFilterTabbarView(
+      title: title,
+      selectedList: selectedJobList,
+      filteringData: fields[selectedJob].toList() ?? [],
+    );
+  }
+}
