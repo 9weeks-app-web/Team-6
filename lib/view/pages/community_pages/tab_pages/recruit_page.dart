4c86caf45c660a3133ed00bd7ce4432fa6034948
Ko-Eunseo <koyk0408@naver.com>
2024-01-10 18:14:37 +0900

    Refactor: 모드에 따라 다른 리스트 렌더링하도록 구현 #49

--- lib/view/pages/community_pages/tab_pages/recruit_page.dart
+++ lib/view/pages/community_pages/tab_pages/recruit_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common.dart';
 import 'package:flutter_app/util/community/recruit_detail_data.dart';
-import 'package:flutter_app/view/widgets/recruit_page_widgets/advertise_widget.dart';
-import 'package:flutter_app/view/widgets/recruit_page_widgets/more_button_widget.dart';
+import 'package:flutter_app/view/widgets/community_page_widgets/chip_widget.dart';
+import 'package:flutter_app/view/widgets/community_page_widgets/default_recruit_list.dart';
+import 'package:flutter_app/view/widgets/community_page_widgets/recruit_search_list.dart';
 import 'package:flutter_app/view/widgets/recruit_page_widgets/recruit_card_widget.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,6 +14,11 @@ class RecruitPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isFilterMode = false;
+    void changeFilterMode() {
+      isFilterMode = !isFilterMode;
+    }
+
     PageController pageController = PageController(
       initialPage: 1,
       viewportFraction: 0.91,
@@ -23,10 +29,17 @@ class RecruitPage extends StatelessWidget {
     double listTileTopPadding = 17.5;
     double tabbarContentGap = 24;
 
+    List<String> filterConditions = [
+      '서울 강남구',
+      '디자인',
+      '3년차',
+      'UX/UI',
+      'UX/UI디자이너',
+    ];
+
     return ListView(
       shrinkWrap: true,
       children: [
-        //recruit card영역
         Padding(
           padding: EdgeInsets.only(
             top: tabbarContentGap - listTileTopPadding,
@@ -34,6 +47,32 @@ class RecruitPage extends StatelessWidget {
           ),
           child: Column(
             children: [
+              Visibility(
+                visible: isFilterMode,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 24),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(maxHeight: 26),
+                      child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: filterConditions.length,
+                        padding: const EdgeInsets.only(right: 20),
+                        itemBuilder: (BuildContext context, int index) {
+                          return ChipWidget(
+                            label: filterConditions[index],
+                            type: ChipType.filtering,
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return const SizedBox(width: 8);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               ListTile(
                 contentPadding: const EdgeInsets.only(right: 20),
                 title: Text(
@@ -81,98 +120,13 @@ class RecruitPage extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 28),
-        Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                contentPadding: const EdgeInsets.only(left: 0, right: 20),
-                title: Text(
-                  '지원자가 많은 회사',
-                  style: DesignTextStyle.SubTitle_Bold,
-                ),
-                trailing: MoreButtonWidget(
-                  onPressed: () {},
-                ),
+        isFilterMode == true
+            ? RecruitSearchList(
+                recruitData: recruitDetailData,
+              )
+            : DefaultRecruitList(
+                recruitData: recruitDetailData,
               ),
-              SizedBox(
-                height: 206,
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(right: 20),
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          context.push(
-                            '/community/recruit/${recruitDetailData[index]['id']}',
-                          );
-                        },
-                        child: RecruitCardWidget(
-                          recruitData: recruitDetailData[index],
-                          type: CardType.minimum,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(height: 28),
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.0),
-          child: AdvertiseWidget(),
-        ),
-        const SizedBox(height: 28),
-        Padding(
-          padding: const EdgeInsets.only(left: 20.0),
-          child: Column(
-            children: [
-              ListTile(
-                contentPadding: const EdgeInsets.only(left: 0, right: 20),
-                title: Text(
-                  '지역별 인기있는 회사',
-                  style: DesignTextStyle.SubTitle_Bold,
-                ),
-                trailing: MoreButtonWidget(
-                  onPressed: () {},
-                ),
-              ),
-              SizedBox(
-                height: 206,
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(right: 20),
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          context.push(
-                            '/community/recruit/${recruitDetailData[index]['id']}',
-                          );
-                        },
-                        child: RecruitCardWidget(
-                          recruitData: recruitDetailData[index],
-                          type: CardType.minimum,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 30),
-            ],
-          ),
-        ),
       ],
     );
   }
