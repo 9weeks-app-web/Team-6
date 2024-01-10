f0a27ec7479dab4fc4289338d8153c8ae2471507
sanghyun8039 <chltkdgus10@naver.com>
2024-01-10 16:57:43 +0900

    add project apply widget

--- lib/view/pages/project_pages/project_page.dart
+++ lib/view/pages/project_pages/project_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common.dart';
 import 'package:flutter_app/util/project/project_tab_list.dart';
+import 'package:flutter_app/view/widgets/project_widgets/project_apply_widget.dart';
 import 'package:flutter_app/view/widgets/project_widgets/tab_widget.dart';
 import 'package:flutter_app/view/widgets/header_widget.dart';
 import 'package:flutter_app/view/widgets/recruit_page_widgets/search_bar_widget.dart';
@@ -10,53 +11,62 @@ class ProjectPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Color(0xFF0059FF),
-        child: const Icon(
-          Icons.edit,
-          color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              useSafeArea: true,
+              isScrollControlled: true,
+              context: context,
+              builder: (context) => const ProjectApplyWidget(),
+            );
+          },
+          backgroundColor: const Color(0xFF0059FF),
+          child: const Icon(
+            Icons.edit,
+            color: Colors.white,
+          ),
         ),
-      ),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(
-                left: 20,
-                right: 20,
-                top: 10,
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 10,
+                ),
+                child: HeaderWidget(),
               ),
-              child: HeaderWidget(),
-            ),
-            const SizedBox(height: 18),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Text(
-                    '프로젝트',
-                    style: DesignTextStyle.HeadLine_Bold,
+              const SizedBox(height: 18),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      '프로젝트',
+                      style: DesignTextStyle.HeadLine_Bold,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 8),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20),
-                  child: SearchBarWidget(),
-                ),
-                const SizedBox(height: 16),
-              ],
-            ),
-            Expanded(
-              child: TabWidget(
-                tabContents: projectTabList,
+                  const SizedBox(height: 8),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: SearchBarWidget(),
+                  ),
+                  const SizedBox(height: 16),
+                ],
               ),
-            ),
-          ],
+              Expanded(
+                child: TabWidget(
+                  tabContents: projectTabList,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
